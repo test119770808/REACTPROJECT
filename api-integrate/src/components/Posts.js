ed6fffa927088de0005ab24678a9fccafade5bda
test119770808 <test119770808@gmail.com>
2024-06-19 15:12:32 +0900

    240619 5교시

--- api-integrate/src/components/Posts.js
+++ api-integrate/src/components/Posts.js
@@ -15,26 +15,44 @@ function Posts() {
 
   const [posts, setPosts] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // postTest 함수 정의 
+  const postTest = async () => {
+    try{
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts'); //결과?
+      // response.data.filter(t => t.id <= 20);
+      console.log(response.status);    //응답 코드
+      setPosts(response.data.filter(t => t.id <= 20));  
+      setLoading(true);
+    }catch(e) {
+      setError(e)
+      console.log(e);
+      console.log(e.message);          // 에러 메시지
+      console.log(e.response.status);  // 응답 코드 
+    }
+  };
 
   useEffect(() => {
-    // postTest 함수 정의 
-    const postTest = async () => {
-      try{
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts1'); //결과?
-        // response.data.filter(t => t.id <= 20);
-        setPosts(response.data.filter(t => t.id <= 20));  
-        setLoading(true);
-      }catch(e) {
-        console.log(e.message);
-      }
-      
-    }
     //함수 실행
-    postTest();    
+    postTest();
   }, []);
+
   // console.log(posts);
+  if (error) return (
+    <div>
+      <h1>에러발생했습니다.</h1>
+      <p>{error.message}</p>
+    </div>
+  );
+
+  if (!loading) return <h1>로딩중입니다...</h1>;
+
   return (
     <div>
+      <button onClick={() => postTest()}>다시불러오기</button>
+      <hr/>
+      {console.log(loading)}
       {loading && posts.map(post => (
         <Post key={post.id} id={post.id} title={post.title} userId={post.userId} />
       ))}
